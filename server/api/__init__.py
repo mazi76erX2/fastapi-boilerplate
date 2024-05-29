5ebcf0ab2b0399b15ea4124681250550fbe68788
Xolani <mazi76erx@gmail.com>
2024-05-29 18:40:36 +0000

    Updated fastapi with cache and latest dependencies

---
+++ server/api/__init__.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+# from .items import router as items_router
+# Import more routers here
+
+api_router = APIRouter()
+
+# Example of including a router
+# api_router.include_router(items_router, prefix="/items", tags=["items"])
+# Add more routers here
